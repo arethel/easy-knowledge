2e5218936e1686a0937e5444bc0586ece0549cb0
Ivan Smirnov <ivan.smirnov@stud.th-deg.de>
2024-01-04 20:42:38 +0100

    Add debug_toolbar

--- easy_knowledge/easy_knowledge/settings.py
+++ easy_knowledge/easy_knowledge/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
-    'frontend.apps.FrontendConfig',
-    'users.apps.UsersConfig',
+
     'rest_framework',
     "corsheaders",
     'django_celery_results',
+    'debug_toolbar',
+    
+    'api.apps.ApiConfig',
+    'frontend.apps.FrontendConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'easy_knowledge.urls'
@@ -128,6 +133,15 @@ STATICFILES_DIRS = [
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- easy_knowledge/easy_knowledge/urls.py
+++ easy_knowledge/easy_knowledge/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from easy_knowledge import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
